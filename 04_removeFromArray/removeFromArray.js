0eb84a2e9dfb278a7c1bc5a3df8d46c2a0a00d72
Marques Taylor <mintyquest@icloud.com>
2022-07-15 16:02:38 -0400

    Finish removeFromArray javascript exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...unwantedValues) {
 
+let i = 0;
+
+    
+    do {
+        
+        if (array.includes(unwantedValues[i]) && typeof unwantedValues[i] == typeof array[array.indexOf(unwantedValues[i])]) {
+            
+            ;
+            array.splice(array.indexOf(unwantedValues[i]), 1);
+            
+            console.log(array);
+        }
+        
+        i++
+    } while (i < unwantedValues.length);
+
+return array;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
