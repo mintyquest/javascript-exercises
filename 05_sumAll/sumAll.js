3cf717e8f1bd2df1dcac9a70384873b3310c2cf4
Marques Taylor <mintyquest@icloud.com>
2022-07-15 17:24:20 -0400

    Finish sumAll javascript exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,34 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
 
+    let currentNumber = firstNum;
+    let currentNumber2 = secondNum;
+    let sum = 0;
+    if (firstNum > -1 && secondNum > -1 && typeof firstNum == "number" && typeof secondNum == "number" ) {
+        
+        if (firstNum < secondNum) {
+            for (i = 0; i < (secondNum - (firstNum - 1)); i++) { //will iterate for the amount of distance there is between the two numbers and the second number is the bigger one.
+                
+                sum += currentNumber;
+                currentNumber++;
+            }
+        }
+        else {
+            for (i = 0; i < (firstNum - (secondNum - 1)); i++) { //will iterate for the amount of distance there is between the two numbers and the first number is the bigger one.
+                
+                sum += currentNumber2;
+                currentNumber2++;
+            }
+        }
+    }
+    else {
+        return 'ERROR';
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+//make a loop using the difference between the first number and the second one
