9a0ce575cfe6b3d5e91869d9864ff67c360d60a2
Marques Taylor <mintyquest@icloud.com>
2022-07-15 00:24:14 -0400

    Finish repeatString javascript exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    
+    
+    let combinedString = '';
+    if (num < 0)  {
+        return 'ERROR';
+    }
+    else {
+        for (i = 0; i < num; i++) {
+        combinedString += string;
+            
+        }
+    }
+    return combinedString;
 };
 
 // Do not edit below this line
