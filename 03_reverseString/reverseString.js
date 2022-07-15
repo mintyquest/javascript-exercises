ee17df897336d1e2ece0012272f976a64886c3e4
Marques Taylor <mintyquest@icloud.com>
2022-07-15 00:49:34 -0400

    Finish reverseString javascript exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let copyString = '';
+    reverseCharLocate = string.length - 1;
+    for (i = 0; i < string.length; i++) {
+        copyString += string.charAt(reverseCharLocate--);
+    }
+    return copyString;
 };
 
 // Do not edit below this line
