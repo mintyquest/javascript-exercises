eabac8213c821611bf94d7a3dcf51dc8a803b2a3
Marques Taylor <mintyquest@icloud.com>
2022-07-15 21:56:17 -0400

    Finish leapYears javascript exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(leapYears) {
 
+    if (leapYears % 400 == 0) { //Approves years that are divisble by 400
+        return true;
+    }
+    else if (leapYears % 4 == 0 && leapYears % 100 != 0) { //Approves years like 1996 but filters years that are divisible by 100 but not by 400
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
