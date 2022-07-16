be539e406c91c82ae6c46e9574fe7b9840da6c06
Marques Taylor <mintyquest@icloud.com>
2022-07-15 22:33:36 -0400

    Finish tempConversion javascript exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
 
+  //return (faren - 32) * parseFloat(5/9).toFixed(1);
+  return +(((fahrenheit - 32) * 5) /9).toFixed(1);
+  
 };
 
-const ctof = function() {
+const ctof = function(celsius) {
 
+  return +((celsius * 9) / 5 + 32).toFixed(1);
 };
 
 // Do not edit below this line
